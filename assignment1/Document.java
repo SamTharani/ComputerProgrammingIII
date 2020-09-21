2513fb3a20c2dabca673d9743e45227745ee736d
samanthajeya@gmail.com <samanthajeya@gmail.com>
2020-09-21 16:12:04 +0530

    Method signature added for the Java class

---
+++ assignment1/Document.java
@@ -0,0 +1,37 @@
+public class Document
+{
+	private int id;
+	private String name;
+	private String content;
+	private String extension;
+	
+	public void setName(String name)
+	{
+		
+	}
+	
+	public void setContent(String content)
+	{
+		
+	}
+	public void setExtension(String extension)
+	{
+		
+	}
+	public String getId()
+	{
+		
+	}
+	public String getName()
+	{
+		
+	}
+	public String getContent()
+	{
+		
+	}
+	public String getExtension()
+	{
+		
+	}
+}
